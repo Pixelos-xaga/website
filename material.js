a55dfea4ed7ee13670a16abed2d3ac36ff96dd6b
Angxddeep <angxddeep@gmail.com>
2026-02-23 20:43:10 +0000

    Fix hero CTA alignment with tonal Material button

--- material.js
+++ material.js
@@ -1,4 +1,5 @@
 import '@material/web/button/filled-button.js';
+import '@material/web/button/filled-tonal-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/chips/assist-chip.js';
 import '@material/web/divider/divider.js';
