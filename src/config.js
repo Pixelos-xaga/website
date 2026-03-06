96e4907d03b6c4c1f4dfa364578953ba232eb6fd
Angxddeep <angxddeep@gmail.com>
2026-03-06 21:13:49 +0530

    update test url

--- src/config.js
+++ src/config.js
@@ -31,27 +31,27 @@ export const TEST_DOWNLOADS = [
   {
     name: 'Test ROM zip',
     note: 'Experimental/Test PixelOS ROM zip file',
-    href: '#'
+    href: 'https://drive.google.com/file/d/1apUQf6JjcBdtsL0FoDjjcpR5Vy-Q-Lvq/view?usp=drive_link'
   },
   {
     name: 'Test boot.img',
     note: 'Experimental/Test boot image',
-    href: '#'
+    href: 'https://drive.google.com/file/d/1bF5eLjb7g1ur8ifNoVyXV5RAfZKCQdWU/view?usp=sharing'
   },
   {
     name: 'Test vendor_boot.img',
     note: 'Experimental/Test vendor boot image',
-    href: '#'
+    href: 'https://drive.google.com/file/d/1zx2VFRxUTwvbbgDHkc9I3qG45CBxMn_u/view?usp=drive_link'
   },
   {
     name: 'Test vbmeta.img',
     note: 'Experimental/Test verified boot metadata',
-    href: '#'
+    href: 'https://drive.google.com/file/d/1C3v_W7eir2kc8zuptFRGmmpPOijMkkbt/view?usp=drive_link'
   },
   {
     name: 'Test dtbo.img',
     note: 'Experimental/Test device tree blob overlay',
-    href: '#'
+    href: 'https://drive.google.com/file/d/1MfjqhzURMZosfjfIb8P0g3Ooft6h2AFy/view?usp=drive_link'
   }
 ];
 
